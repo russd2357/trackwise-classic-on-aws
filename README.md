c09693ffadf3adaef0a93e9f6b06eb50a87c722b
de Pina <pinaruss@38f9d3534cf6.ant.amazon.com>
2021-08-19 22:09:25 -0500

    Added VPC to dev stack

--- README.md
+++ README.md
@@ -12,3 +12,9 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
  * `cdk deploy`      deploy this stack to your default AWS account/region
  * `cdk diff`        compare deployed stack with current state
  * `cdk synth`       emits the synthesized CloudFormation template
+
+
+ # Prerequisites
+
+ You should configure Host Management for AWS Systems Manager so that an IAM role is created to allow
+ access to your EC2 instances on private subnets. Otherwise you will have to add a bastion host in a public subnet to reach them which will add to your deployment cost.
--- lib/twc-dev-stack.ts
+++ lib/twc-dev-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/lib/aws-ec2';
 import { Construct } from 'constructs';
 
 
@@ -6,10 +7,35 @@ import { Construct } from 'constructs';
 // TWC Dev environment
 //
 export class TwcDevStack extends cdk.Stack {
+
+    private vpc: ec2.Vpc;
+
     constructor(scope: Construct, id: string, props?: cdk.StackProps ) {
         super(scope, id, props);
 
-        
+        this.vpc = new ec2.Vpc(this, 'dev-vpc', {
+            cidr: '10.0.0.0/16',
+            maxAzs: 2,
+            natGateways: 1,     
+            subnetConfiguration: [
+                {   // public subnet for ALB (note ALB must point at 2 public subnets in different AZs)
+                    name: 'sn-pub',
+                    subnetType: ec2.SubnetType.PUBLIC,
+                    cidrMask: 24
+                },
+                {   // private subnet for app server
+                    name: 'sn-priv-app',
+                    subnetType: ec2.SubnetType.PRIVATE,
+                    cidrMask: 24
+                },
+                {   // private subnet for db server
+                    name: 'sn-priv-db',
+                    subnetType: ec2.SubnetType.PRIVATE,
+                    cidrMask: 24
+                }
+            ]
+        });
+
 
     }
 }
